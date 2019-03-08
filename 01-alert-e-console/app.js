8535e36f36e6cd6de08a406f1adc16f32b0e9af5
marcelopoars <marcelo.fatecpoa@gmail.com>
2019-03-08 07:56:43 -0300

    Primeiros testes com JavaScript.

---
+++ 01-alert-e-console/app.js
@@ -0,0 +1,12 @@
+        // Imprime mensagem na tela
+        alert('Primeira mensagem ao usuário!')
+        alert('############### Segunda mensagem ao usuário!')
+
+        // Criando variável "nome"
+        // Recebendo dado do usuário
+        var nome = prompt('Digite o seu nome: ')
+
+        alert('Bem vindo '+ `${nome}`) // Formatação ES6
+
+        // Imprime mensagem via console
+        console.log('Mensagem via console.')
