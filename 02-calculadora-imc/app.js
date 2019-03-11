079b763c4655faec8958fa39af90818a366235e5
marcelopoars <marcelo.fatecpoa@gmail.com>
2019-03-10 21:01:10 -0300

    Calculando IMC com JS.

---
+++ 02-calculadora-imc/app.js
@@ -0,0 +1,15 @@
+// Linha de comentário no JavaScritp
+
+// Declarando variáveis
+var peso = 69
+var altura = 1.69
+
+// Estrutura condifional IF e ELSE
+if(altura != 0) {
+    var imc = peso / (altura * altura);
+    console.log(imc)
+}
+
+else {
+    console.log('Erro: A altura deve ser diferente de zero.')
+}
