848f74e5285793c3c7f97456f1a60b2f1545f252
Marcelo Pereira <marcelo.fatecpoa@gmail.com>
2019-10-10 23:08:22 -0300

    Condições...

---
+++ 10-condicoes/condicao-composta.js
@@ -0,0 +1,10 @@
+let pais = 'Brasil'
+
+console.log(`Vivendo em ${pais}`)
+
+// Condição composta - "if e else"
+if (pais == 'Brasil') {
+    console.log('Brasileiro')
+} else {
+    console.log('Estrangeiro')
+}
---
+++ 10-condicoes/condicao-simples.js
@@ -0,0 +1,11 @@
+console.log('Testando o plugin NODE Exec ....') // F8
+
+// Estudando condições
+//  If e else
+
+let velocidade = 60.5
+console.log(`A velocidade do seu carro é ${velocidade}Km/h.`)
+if (velocidade > 60) { // Condição simples - apenas "if"
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO.`)
+}
+console.log(`Use sempre cinto de segurança!`)
