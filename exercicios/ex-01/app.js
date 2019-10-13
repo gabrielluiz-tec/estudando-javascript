d52004ede7b8d64d51ec7c4ddbc107df524be64a
Marcelo Pereira <marcelo.fatecpoa@gmail.com>
2019-10-13 11:14:46 -0300

    Exercícios js.

--- exercicios/ex-01/app.js
+++ exercicios/ex-01/app.js
@@ -7,31 +7,39 @@ function carregar() {
     
     // Pegando hora atual
     let data = new Date()
-    // let horaAtual = data.getHours()
-    let horaAtual = 1
+    let horaAtual = data.getHours()
 
     // Mensagem ao usuáro
     if (horaAtual >= 0 && horaAtual < 12) {
 
+        // Saudação ao usuário
         saudacao.innerHTML = '<strong>Bom dia!</strong> <br>'
+        // Alterando a imagem conforme hora atual
         imagem.src = 'images/tumb-manha.png'
+        // Alterando plano de fundo conforme hora atual
         document.body.className = 'bg bg-manha'
 
     } else if (horaAtual >= 12 && horaAtual <= 18) {
 
+        // Saudação ao usuário
         saudacao.innerHTML = '<strong>Boa tarde!</strong> <br>'
+        // Alterando a imagem conforme hora atual
         imagem.src = 'images/tumb-tarde.png'
+        // Alterando plano de fundo conforme hora atual
         document.body.className = 'bg bg-tarde'
 
     } else {
 
+        // Saudação ao usuário
         saudacao.innerHTML = '<strong>Boa noite!</strong> <br>'
+        // Alterando a imagem conforme hora atual
         imagem.src = 'images/tumb-noite.png'
+        // Alterando plano de fundo conforme hora atual
         document.body.className = 'bg bg-noite'
 
     }
 
-    // FInalizando saudação ao usuário
+    // Finalizando saudação ao usuário
     saudacao.innerHTML += `Agora são ${horaAtual} horas.`
 
 }
