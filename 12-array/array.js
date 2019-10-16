1bab1f0d3d475f566293fd660b5b73528fdb8b9c
Marcelo Pereira <marcelo.fatecpoa@gmail.com>
2019-10-15 21:09:49 -0300

    Jogo.

---
+++ 12-array/array.js
@@ -0,0 +1,35 @@
+let lista = [3, 7, 99, 8]
+
+// Desta forma imprime os colchetes "[]"
+console.log(lista)
+
+// Desta forma imprime sem os colchetes "[]"
+console.log(`Imprimindo a lista: ${lista}`)
+
+// Inserindo novo valor no array
+lista[4] = 21
+lista[10] = 'casa'
+
+// Usando o método interno "push"
+// Adiciona o valor na última posição do array
+lista.push(333)
+
+console.log(`Imprimindo a lista: ${lista}`)
+console.log(`Imprimindo um elemento: ${lista[1]}`)
+
+// Descobrindo o tamanho do array com o "atributo - length"
+// Atenção: não vai "parenteses" pois é um método
+console.log(`Tamanho do array: ${lista.length}`)
+
+// Ordenando o array com o método "sort()"
+lista.sort()
+
+console.log(`Imprimindo a lista ordenada: ${lista}`)
+
+// Imprimindo array com "for"
+for (let pos in lista) {
+    console.log(`${pos} - ${lista[pos]}`)
+}
+
+// Verificar se valor existe no array
+console.log(lista.includes('casa'))
---
+++ jogo/app.js
@@ -0,0 +1,57 @@
+
+let contador = 0
+
+let tentativas = document.querySelector('#tentativas')
+let ultimoResultado = document.querySelector('#ultimoResultado')
+let listaNumeros = []
+// Gera um número aleatório entre "1 e 100"
+let numeroAleatorio = Math.floor(Math.random() * 100) + 1;
+// Mostra no console o número gerado pelo sistema
+console.log(`Número Aleatório: ${numeroAleatorio}`)
+
+let botao = document.querySelector('#botaoTentar')
+
+
+function advinhaNumero() {
+    let tentativa = Number(document.querySelector('#tentativa').value)
+
+    if (tentativa == 0 || tentativa < 1 || tentativa > 100) {
+        baixoAlto.innerHTML = 'Por favor,<br>digite um número entre <strong>1</strong> e <strong>100</strong>.'
+    } else {
+        if (tentativa == numeroAleatorio) {
+            contador++
+            tentativas.innerHTML = ''
+            ultimoResultado.innerHTML = ''
+            botao.innerHTML = '<a href="/">Jogar novamente</a>'
+            baixoAlto.innerHTML = `<strong>Parabéns!</strong><br>Você acertou em <strong>${contador}</strong> tentativas.`
+            contador = 0
+            listaNumeros = []
+        } else {
+            if (listaNumeros.includes(tentativa)) {
+                baixoAlto.innerHTML = 'Este número já foi digitado.'
+                console.log(`${tentativa} está no array`)
+            } else {
+                if (tentativa < numeroAleatorio) {
+                    baixoAlto.innerHTML = 'BAIXO. Digite um número maior.'
+                } else {
+                    baixoAlto.innerHTML = 'ALTO. Digite um número menor.'
+                }
+                contador++
+                tentativas.innerHTML = `Tentativas: ${contador} <br>`
+                ultimoResultado.innerHTML += `[<strong>${tentativa}</strong>] `
+                listaNumeros.push(tentativa)
+
+                if (contador == 10) {
+                    botao.innerHTML = '<a href="/">Jogar novamente</a>'
+                    tentativas.innerHTML = ''
+                    ultimoResultado.innerHTML = ''
+                    botao.innerHTML = '<a href="/">Jogar novamente</a>'
+                    baixoAlto.innerHTML = `<strong>Que pena!</strong><br>Tente novamente.`
+                }
+            }
+        }
+        console.log(listaNumeros)
+        console.log(`Contador: ${contador}`)
+        // console.log(`Número Aleatório: ${numeroAleatorio}`)
+    }
+} // Fim da função
