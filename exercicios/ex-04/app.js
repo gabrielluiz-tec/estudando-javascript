3bc4f763d6a7a2e4a72e6eb56a27c1fb72b90f02
Marcelo Pereira <marcelo.fatecpoa@gmail.com>
2019-10-16 23:40:03 -0300

    Correções na lógica do Exercício 04.

--- exercicios/ex-04/app.js
+++ exercicios/ex-04/app.js
@@ -1,65 +1,51 @@
 let numero = document.querySelector('#numero')
 let resultado = document.querySelector('#resultado')
-let analise = document.querySelector('#analise')
 let erros = document.querySelector('#erros')
 let listaNumeros = []
 
 function adicionarNumero() {
     if (!validarNumero(Number(numero.value))) {
-        analise.innerHTML = ''
         erros.innerHTML = ''
         listaNumeros.push(Number(numero.value))
         resultado.innerHTML += `<span>${numero.value}</<span>`
-        numero.value = ''
-        numero.focus()
-        console.log(listaNumeros)
-    }
+    } limparInput()
 }
 
 function validarNumero(num) {
     if (num < 1 || num > 100 ) {
         erros.innerHTML = `Número inválido`
-        numero.value = ''
-        numero.focus()
-        analise.innerHTML = ''
+        limparInput()
+        resultado.innerHTML = ''
         return true
     }
     else if (listaNumeros.includes(num)) {
         erros.innerHTML = `Número já cadastrado`
-        numero.value = ''
-        numero.focus()
+        limparInput()
         return true
     }
 }
 
-function analisarNumero(listaNum) {
-    analise.innerHTML = ''
+function analisarNumero() {
     resultado.innerHTML = ''
     erros.innerHTML = ''
     if (listaNumeros.length == 0) {
         erros.innerHTML = `Nenhum número cadastrado`
-        numero.value = ''
-        numero.focus()
     } else {
-        analise.innerHTML = `Lista com <strong>${listaNumeros.length}</strong> números<br>`
         let maior = Math.max(...listaNumeros)
-        analise.innerHTML += `O maior valor é: <strong>${maior}</strong><br>`
         let menor = Math.min(...listaNumeros)
-        analise.innerHTML += `O menor valor é: <strong>${menor}</strong><br>`
         let soma = listaNumeros.reduce( (accum, curr) => accum + curr )
-        analise.innerHTML += `A soma dos valores é: <strong>${soma}</strong><br>`
         let media = soma / listaNumeros.length
-        analise.innerHTML += `A média dos valores é: <strong>${media.toFixed(2)}</strong><br>`
+        resultado.innerHTML = `Lista com <strong>${listaNumeros.length}</strong> números<br>`
+        resultado.innerHTML += `O maior valor é: <strong>${maior}</strong><br>`
+        resultado.innerHTML += `O menor valor é: <strong>${menor}</strong><br>`
+        resultado.innerHTML += `A soma dos valores é: <strong>${soma}</strong><br>`
+        resultado.innerHTML += `A média dos valores é: <strong>${media.toFixed(2)}</strong><br>`
         listaNumeros = []
-        numero.value = ''
-        numero.focus()
-        console.log(listaNumeros)
-    }
+    } limparInput()
 }
 
-// Verifica se tecla enter foi pressionada
 document.addEventListener('keypress', function(e){
-    if(e.which == 13){
-        adicionarNumero()
-    }
+    if(e.which == 13){ adicionarNumero() }
   }, false);
+
+  function limparInput() { numero.value = ''; numero.focus() }
